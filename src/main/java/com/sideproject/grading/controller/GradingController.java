21063c6ff0536925f91a6c3eac7811ed3947aedd
suhyeoonn <suhyeon1740@gmail.com>
2024-07-13 14:40:13 +0900

    시작, 범위 선택 페이지 컨트롤러 추가

---
+++ src/main/java/com/sideproject/grading/controller/GradingController.java
@@ -0,0 +1,17 @@
+package com.sideproject.grading.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GradingController {
+    @GetMapping("/")
+    public String start() {
+        return "start";
+    }
+
+    @GetMapping("/range-selection")
+    public String rangeSelection() {
+        return "range-selection";
+    }
+}
