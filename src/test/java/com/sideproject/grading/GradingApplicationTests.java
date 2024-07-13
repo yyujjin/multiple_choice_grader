ef60181f23d37f4840acfb596945bf6b797b294b
suhyeoonn <suhyeon1740@gmail.com>
2024-07-13 13:58:08 +0900

    스프링부트 초기 설정 파일 추가

---
+++ src/test/java/com/sideproject/grading/GradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.grading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
