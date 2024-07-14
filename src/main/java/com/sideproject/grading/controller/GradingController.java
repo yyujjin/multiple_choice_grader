243bc5562d177f4c3869f2aff1a0b1710d09c7d0
suhyeoonn <suhyeon1740@gmail.com>
2024-07-14 15:01:12 +0900

    답안 선택 페이지 추가

--- src/main/java/com/sideproject/grading/controller/GradingController.java
+++ src/main/java/com/sideproject/grading/controller/GradingController.java
@@ -1,7 +1,11 @@
 package com.sideproject.grading.controller;
 
+import com.sideproject.grading.domain.Question;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class GradingController {
@@ -13,5 +17,18 @@ public class GradingController {
     @GetMapping("/range-selection")
     public String rangeSelection() {
         return "range-selection";
+    }
+
+    @GetMapping("/answer-selection")
+    public String answerSelection(Model model) {
+        ArrayList questions = new ArrayList<>();
+        questions.add(new Question(1));
+        questions.add(new Question(2));
+        questions.add(new Question(5));
+
+//        [{chapter: 1, numbers: [1,2,3]}]
+        model.addAttribute("questions", questions);
+
+        return "answer-selection";
     }
 }
