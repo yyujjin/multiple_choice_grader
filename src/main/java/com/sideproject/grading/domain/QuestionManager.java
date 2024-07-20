a1f8e847c991c7c59728660e887c6e3a419b60c5
yyujjin <aba1740@naver.com>
2024-07-20 15:53:50 +0900

    :memo: 변수 명 변경

--- src/main/java/com/sideproject/grading/domain/QuestionManager.java
+++ src/main/java/com/sideproject/grading/domain/QuestionManager.java
@@ -1,20 +1,18 @@
 package com.sideproject.grading.domain;
 
-import lombok.Data;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class QuestionManager {
 
     //배열 전역변수로 만들기
-    private  static Map<Integer, Question> selectedScrapedAnswers = new HashMap<Integer, Question>();
+    private  static Map<Integer, Question> selectedScrapAnswers = new HashMap<Integer, Question>();
 
-    public static Map<Integer, Question> getSelectedScrapedAnswers() {
-        return selectedScrapedAnswers;
+    public static Map<Integer, Question> getSelectedScrapAnswers() {
+        return selectedScrapAnswers;
     }
 
-    public static void setSelectedScrapedAnswers(Map<Integer, Question> selectedScrapedAnswers) {
-        QuestionManager.selectedScrapedAnswers = selectedScrapedAnswers;
+    public static void setSelectedScrapAnswers(Map<Integer, Question> selectedScrapedAnswers) {
+        QuestionManager.selectedScrapAnswers = selectedScrapedAnswers;
     }
 }
