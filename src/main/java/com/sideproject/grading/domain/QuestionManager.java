1dfe8b30bffad07dfe67cb942ea80044dc2da6f9
yyujjin <aba1740@naver.com>
2024-07-20 14:56:52 +0900

    :sparkles: getter, setter 생성

--- src/main/java/com/sideproject/grading/domain/QuestionManager.java
+++ src/main/java/com/sideproject/grading/domain/QuestionManager.java
@@ -5,9 +5,16 @@ import lombok.Data;
 import java.util.HashMap;
 import java.util.Map;
 
-@Data
 public class QuestionManager {
 
     //배열 전역변수로 만들기
     private  static Map<Integer, Question> selectedScrapedAnswers = new HashMap<Integer, Question>();
+
+    public static Map<Integer, Question> getSelectedScrapedAnswers() {
+        return selectedScrapedAnswers;
+    }
+
+    public static void setSelectedScrapedAnswers(Map<Integer, Question> selectedScrapedAnswers) {
+        QuestionManager.selectedScrapedAnswers = selectedScrapedAnswers;
+    }
 }
