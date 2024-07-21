376bbaf556df2f1dcf28953f5858dc8eaa87ff49
yyujjin <aba1740@naver.com>
2024-07-22 00:57:45 +0900

    :sparkles: 틀린 문제가 없을 경우 알림창 출력 및 링크 이동 방지 기능 구현

--- src/main/resources/static/js/result.js
+++ src/main/resources/static/js/result.js
@@ -1,8 +1,21 @@
 const $exit = document.querySelector('#exit')
-
+const $wrong = document.querySelector("#wrong")
 $exit.addEventListener('click', (e) => {
     if (!confirm('시작 화면으로 돌아가시겠습니까?')) {
         e.preventDefault()
         return
     }
 })
+
+$wrong.addEventListener("click",event => {
+    if(correctAnswerCount==totalCount){
+        event.preventDefault();
+        alert("틀린문제가 없습니다!")
+    }else{
+        window.location.href = $wrong.href;
+    }
+
+})
+
+console.log(totalCount)
+console.log(correctAnswerCount)
