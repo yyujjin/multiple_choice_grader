7dc90955e853a9a96e5f98689dd19f1b91161f04
yyujjin <aba1740@naver.com>
2024-07-23 17:49:35 +0900

    자료형 String-> enum으로 변경

--- src/main/java/com/sideproject/grading/domain/Question.java
+++ src/main/java/com/sideproject/grading/domain/Question.java
@@ -2,33 +2,14 @@ package com.sideproject.grading.domain;
 
 
 public class Question {
-    //문제 번호가 필요있는지 몰라서 일단 주석 처리
-    //private int number;
-    //알쏭달쏭,모르겠다
-    //중복 선택 불가능하니 하나로 처리
-    private String scrapeType;
-    private boolean isScrap;
 
-    public Question (String scrapeType,boolean isScrap ) {
+    private ScrapeType scrapeType;
+
+    public Question (ScrapeType scrapeType) {
         this.scrapeType = scrapeType;
-        this.isScrap = isScrap;
     }
 
-
-    public String getScrapeType() {
+    public ScrapeType getScrapeType() {
         return scrapeType;
     }
-    public boolean getIsScrap(){return isScrap;}
-
-    @Override
-    public String toString() {
-        return "Question{" +
-                "scrapeType='" + scrapeType + '\'' +
-                ", isScrap=" + isScrap +
-                '}';
-    }
-
-    // public int getNumber() {
-     //   return number;
-    //}
 }
